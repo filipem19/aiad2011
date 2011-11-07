0299bbf2e952f6c7ff5ce8384d1eb9a1cc97ef09
joaogbrito <joaogbrito@gmail.com>
2011-11-07 12:00:29 +0000

    System management integration (agent, products, operation initialization) INCOMPLETE

--- AGV's/src/systemManagement/SystemManager.java
+++ AGV's/src/systemManagement/SystemManager.java
@@ -3,6 +3,13 @@ package systemManagement;
 import jade.gui.GuiAgent;
 import jade.gui.GuiEvent;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+import systemManagement.SystemManagerGUI.ObjectType;
+
 public class SystemManager extends GuiAgent{
 
 	/**
@@ -15,10 +22,72 @@ public class SystemManager extends GuiAgent{
 	
 	@Override
 	protected void setup() {
-//		myGui = new SystemManagerGUI();
+		loadProgramData("ProgramData");
 		
+		myGui = new SystemManagerGUI();
 	}
 
+	/**
+	 * Load data from file and creates instances of objects specified in this file
+	 * @param filename
+	 */
+	private void loadProgramData(String filename) {
+		try{
+			FileInputStream fis = new FileInputStream(filename);
+			DataInputStream	in = new DataInputStream(fis);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			ObjectType objectType = null;
+			while((strLine = br.readLine()) != null){
+				System.out.println(strLine);
+				//file decoding and object creation
+				objectType = decodeLine(strLine, objectType);
+			}
+			in.close();
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			System.err.println("error: " + e.getMessage());
+			System.out.println("erro na leitura do ficheiro:" + filename);
+		}
+	}
+
+	private ObjectType decodeLine(String strLine, ObjectType objectType) {
+		String[] parts = strLine.split(":");
+		ObjectType currentType = null;
+		if(parts.length == 1){
+			currentType = identifyObjectType(parts[0],currentType);
+			if(currentType == null){
+				System.err.println("error parsing file : File corrupt");
+			}
+		}
+		else{
+			createObject(parts, objectType);
+		}
+		
+		return currentType;
+	}
+
+	private void createObject(String[] parts, ObjectType objectType) {
+		// TODO Auto-generated method stub
+				
+	}
+
+	private ObjectType identifyObjectType(String string, ObjectType currentType) {
+		
+		if(string.compareTo("Machines") == 0)
+			return ObjectType.Machine;
+		
+		if(string.compareTo("Products") == 0)
+			return ObjectType.Product;
+		
+		if(string.compareTo("Operations") == 0)
+			return ObjectType.Operation;
+	
+		return null;
+	}
+
+	
 	@Override
 	protected void onGuiEvent(GuiEvent arg0) {
 		// TODO Auto-generated method stub
--- AGV's/src/systemManagement/SystemManagerGUI.java
+++ AGV's/src/systemManagement/SystemManagerGUI.java
@@ -1,17 +1,44 @@
 package systemManagement;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Vector;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+
+import products.Operation;
+import products.Product;
+
+import agents.Machine;
 
 public class SystemManagerGUI extends JFrame{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6978147188402748454L;
+
+	private Vector<Machine> machines;
+	private Vector<Product> products;
+	private Vector<Operation> operations;
+	
+	public enum ObjectType{Machine, Operation, Product};
+	
 	public SystemManagerGUI() {
-		initializeWindowPreferences();
+		//initializeWindowPreferences();
 	}
 	
+
 	private void initializeWindowPreferences(){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Initial platform");
 		setVisible(true);
 		setSize(200, 200);
 		setLocation(200, 400);
+		JButton button = new JButton("Adicionar uma cena");
+		add(button);
 	}
 }
