3c95e7346d74e067feb5677c1991ed7278185e29
joaogbrito@gmail.com <joaogbrito@gmail.com@48d0a4a9-5d2d-2f1c-2275-32473cd17f34>
2011-11-07 12:00:29 +0000

    System management integration (agent, products, operation initialization) INCOMPLETE

--- trunk/AGV's/src/negotiationEngine/MachineToAgvCfpContractResponder.java
+++ trunk/AGV's/src/negotiationEngine/MachineToAgvCfpContractResponder.java
@@ -6,9 +6,6 @@ import jade.proto.ContractNetResponder;
 
 public class MachineToAgvCfpContractResponder extends ContractNetResponder {
 
-		/**
-	 * 
-	 */
 	private static final long serialVersionUID = 7234876748641624186L;
 
 	public MachineToAgvCfpContractResponder(Agent a, MessageTemplate mt) {
