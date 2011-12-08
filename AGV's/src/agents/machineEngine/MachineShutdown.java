a8f49d70f1da35a6352e1684f33d552c8c971cc4
a.isabel.marques <a.isabel.marques@gmail.com>
2011-12-08 10:58:10 +0000

    -gui com adicao de agv e maq

--- AGV's/src/agents/machineEngine/MachineShutdown.java
+++ AGV's/src/agents/machineEngine/MachineShutdown.java
@@ -4,25 +4,29 @@ import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
-public class MachineShutdown extends CyclicBehaviour{
+public class MachineShutdown extends CyclicBehaviour {
 
 	private static final long serialVersionUID = 8198486440722811616L;
+
 	public MachineShutdown(Machine machine) {
 		super(machine);
 	}
-	
-		@Override
-		public void action() {
-			
-			ACLMessage msg = myAgent.receive(MessageTemplate.MatchPerformative(ACLMessage.INFORM));
-			System.out.println(myAgent.getLocalName() + ": msg > " + msg + " -> class = " + myAgent.getClass());
-			if(msg != null && myAgent.getClass() == Machine.class){
-				System.out.println(myAgent.getLocalName() + "message : " + msg.getContent());
-				if(msg.getContent().compareTo("TAKE_DOWN") == 0){
-					((Machine)myAgent).shutdownAgent();
-					System.out.print("vou-me desligar " + myAgent.getName() + "\n");
-				}
-			}
-			block();
+
+	@Override
+	public void action() {
+
+		ACLMessage msg = myAgent.receive(MessageTemplate
+				.MatchPerformative(ACLMessage.INFORM));
+		System.out.println(myAgent.getLocalName() + ": msg > " + msg
+				+ " -> class = " + myAgent.getClass());
+
+		System.out.println(myAgent.getLocalName() + "message : "
+				+ msg.getContent());
+		if (msg.getContent().compareTo("TAKE_DOWN") == 0) {
+			((Machine) myAgent).shutdownAgent();
+			System.out.print("vou-me desligar " + myAgent.getName() + "\n");
 		}
+		block();
+	}
+
 }
