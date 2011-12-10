fe70c68501ffdb6260a726a0c35e6ea561ad51bd
paulo.f.reis@gmail.com <paulo.f.reis@gmail.com@48d0a4a9-5d2d-2f1c-2275-32473cd17f34>
2011-12-10 02:08:54 +0000

    Criado "mapa", extends JPanel. Recebe 2 Arrays de duas dimensões (2 x n) em que n é a quantidade de AGVs ou Máquinas, A posição 0 corresponde à posição X do agente, a posição 1 é a posição Y do agente.
    Apresenta as máquinas e os AGV num painel da GUI.
    
    Falta agora o código para ler a posição X e Y de todos os agentes.

--- trunk/AGV's/src/systemManagement/mapa.java
+++ trunk/AGV's/src/systemManagement/mapa.java
@@ -1,27 +1,50 @@
 package systemManagement;
 
 import java.awt.*;
-import java.awt.geom.*;
 import javax.swing.JPanel;
 
 public class mapa extends JPanel {
 	private static final long serialVersionUID = -1038095695342942324L;
-	private Ellipse2D.Double circle = new Ellipse2D.Double(10, 10, 350, 350);
-	private Rectangle2D.Double square = new Rectangle2D.Double(10, 10, 350, 350);
 	
-	@Override
-	public void paintComponent(Graphics g) {
-		clear(g);
-		Graphics2D g2d = (Graphics2D)g;
-		g2d.fill(circle);
-		g2d.draw(square);
+	private int agvSize = 10;
+	private int machineSize = 20;
+	public int[][] AgvLoc;
+	public int[][] MachineLoc;
+
+    public mapa(int[][] agvLoc, int[][] machineLoc) {
+		AgvLoc = agvLoc;
+		MachineLoc = machineLoc;
 	}
 
-	protected void clear(Graphics g) {
-		super.paintComponent(g);
-	}
+	// Desenho do AGV
+    private void drawAgv(Graphics g, int x, int y, int size) {
+    	g.fillOval(x, y, size, size);
+    }
 	
-	protected Ellipse2D.Double getCircle() {
-		return(circle);
-	}
+    // Desenho da Maquina
+    private void drawMachine(Graphics g, int x, int y, int size) {
+    	g.fillRect(x, y, size, size);
+    }
+ 
+	@Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(
+           RenderingHints.KEY_ANTIALIASING,                
+           RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setColor(Color.BLACK);
+      
+        // Desenhar os AGVs 
+        g.setColor(Color.MAGENTA);
+    	for (int i = 0; i <= AgvLoc.length / 2; i++) {
+        	drawAgv(g, AgvLoc[i][0] * this.getWidth() / 100 , AgvLoc[i][1] * this.getHeight() / 100, agvSize);
+    	}
+        
+        // Desenhar as Máquinas
+        g.setColor(Color.RED);
+    	for (int i = 0; i <= MachineLoc.length / 2; i++) {
+    		drawMachine(g, MachineLoc[i][0] * this.getWidth() / 100 , MachineLoc[i][1] * this.getHeight() / 100, machineSize);
+    	}
+    }
 }
